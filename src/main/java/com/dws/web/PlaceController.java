70f4db8f33d1e3d86fed91d6808ee9db1386b8f5
jrhjavier <jrhjavier2@gmail.com>
2022-03-07 11:18:24 +0100

    PlaceController

--- src/main/java/com/dws/web/PlaceController.java
+++ src/main/java/com/dws/web/PlaceController.java
@@ -16,19 +16,4 @@ public class PlaceController {
         model.addAttribute("sitios", eventHolder);
         return "tablon";  //da error aqui no sabemos por que
     }
-
-    @RequestMapping("/place/tourism")
-    public String tourism(Model model){
-        return "tourism_greeting";
-    }
-
-    @RequestMapping("/place/restaurants")
-    public String restaurant(Model model){
-        return "restaurants_greeting";
-    }
-
-    @RequestMapping("/place/leisure")
-    public String leisure(Model model){
-        return "leisure_greeting";
-    }
 }
