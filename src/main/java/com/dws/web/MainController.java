1553936b6df58c7cc78f54e259f97af10c52f161
jrhjavier <jrhjavier2@gmail.com>
2022-03-08 10:11:34 +0100

    MainController

--- src/main/java/com/dws/web/MainController.java
+++ src/main/java/com/dws/web/MainController.java
@@ -2,7 +2,6 @@ package com.dws.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
